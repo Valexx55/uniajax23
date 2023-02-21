c24e27a79fb757ef49f06bd5253c317d4edf5ed2
valexx55 <valexx55@gmail.com>
2023-02-21 16:27:11 +0100

    martes 1/2 informe selector

--- informe/informe.js
+++ informe/informe.js
@@ -18,13 +18,24 @@ cargarDatos();//lo voy a ejecutar desde node
 function cargarDatos ()
 {
 fetch (URL_DATOS_MUNICIPIOS)
-.then (respuesta=> respuesta.json())
+.then (respuesta=>  
+    {if (respuesta.ok)
+        {
+            return respuesta.json()
+        }
+} )
 .then ((datos) => 
 {
     console.log(datos.data.length);
     console.log(datos.data[0].municipio_distrito);
-    //TODO: 1) extrar un array con las localidades / distritos
-    //2 otra array con las fechas
+    let array_localidades = extraerLocalidades(datos.data);
+    //let array_localidades2 = extraerLocalidades2(datos.data);
+    console.log(`LOCALIDADES = ${array_localidades} TOTAL LOCALIDADES = ${array_localidades.length}`);
+    //console.log(`LOCALIDADES = ${array_localidades2} TOTAL LOCALIDADES = ${array_localidades2.length}`);
+    extraerFechas(datos.data);
+    //TODO: 
+    //3 selector con localidades
+    dibujarSelect(array_localidades);
     //seleccionar una localidad
     //ESTADÍSTICOS
         //máximo Tasa Incidencia 
@@ -34,4 +45,102 @@ fetch (URL_DATOS_MUNICIPIOS)
         
 })
 .catch(error=> {console.log(error);});
+}
+
+function extraerLocalidades (datos)
+{
+    let array_localidades = [];
+    //OBTENER UNA FECHA INICIAL
+    let fecha_inicial = datos [0].fecha_informe;
+    let fecha_nueva=false;
+    let contador = 0;
+    let localidad_aux;
+        //TODO: extraer todas las localidades distitnas de datos
+        do{
+             localidad_aux = datos[contador].municipio_distrito; //RECORRER EL ARRAY DE DATOS
+             array_localidades.push(localidad_aux);//EXTRAYENDO LA LOCALIDAD
+             contador++;
+             fecha_nueva = datos[contador].fecha_informe != fecha_inicial;
+        }while (!fecha_nueva) //HASTA QUE CAMBIE LA FECHA
+
+    return array_localidades;
+}
+
+function extraerLocalidades2 (datos)
+{
+    let array_localidades = [];
+    let fecha_inicial = datos [0].fecha_informe;
+        //1 me quedo con los registros que sean de esta fecha -filter
+        //2 transformo "adelgazo" los registros, extrayendo sólo el municipio_distrito - map
+        array_localidades = datos.filter(registro => registro.fecha_informe==fecha_inicial).map(registro=>registro.municipio_distrito);
+
+    return array_localidades;
+}
+
+function extraerFechas (datos)
+{
+    let listado_fechas = [];
+    let listado_fechas_unico = [];
+
+        //1 me voy a quedar con las fechas "fecha_informe": "2022/03/29 11:51:00"
+        listado_fechas = datos.map(registro => registro.fecha_informe.substr(0,10));
+        console.log('long fechas repes ' + listado_fechas.length);
+        //2 voy a filtrar las distintas
+        listado_fechas_unico = [...new Set(listado_fechas)];//https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Operators/Spread_syntax sintaxis extendida array a parámetros sueltos
+        console.log('long fechas unicas ' + listado_fechas_unico.length);
+
+    return listado_fechas_unico;
+}
+
+function dibujarSelect(array_localidades)
+{
+    //TODO: desplegar localidades
+    let select_element = document.createElement('select');
+    let elemento_option_aux;
+    array_localidades.forEach (
+        localidad =>
+        {
+            elemento_option_aux=  document.createElement('option');
+            elemento_option_aux.value = localidad;
+            elemento_option_aux.innerHTML = localidad;
+            select_element.append(elemento_option_aux);
+        }
+    );
+    let elemento_div_padre = document.getElementById('div_localidades');
+    elemento_div_padre.append(select_element);
+    //recorrer el array de localidades
+        //para cada localidad, 
+        //creo un option
+        //lo añado al select
+}
+
+function calcular()
+{
+    //obtener la localidad seleccionada
+    let localidad_seleccionada = document.getElementsByTagName('select')[0].value;
+    console.log(localidad_seleccionada);
+    //sacar el radio seleccionado
+    //hacer el cáclculo correspondiente
+    //mostrar
+}
+
+function mediaTia ()
+{
+    let media = 0;
+
+    return media;
+}
+
+function maxTia ()
+{
+    let maximo = 0;
+
+    return maximo;
+}
+
+function minTia ()
+{
+    let minimo = 0;
+
+    return minimo;
 }
